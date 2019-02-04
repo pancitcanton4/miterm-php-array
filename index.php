4acdc9238d1777c94e4ea0bc6360e8d5cae62801
Ej <ejtan1997@gmail.com>
2019-02-04 14:52:11 +0800

    second commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+$age = array("Peter"=>"35", "Ben"=>"35", "Joe"=>"43");
+  foreach($age as $name =>$age_me){
+      echo "Name" .$name. ,",Age".$age_me;
+      echo"<br>"
+      echo"</br>"
+  }
+?>
